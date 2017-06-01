bb62293d8b14f4e2e3b43ff92195546ad30a01cb
David Wilson and Marilyn Carlin <david+marilyn@>
2017-06-01 09:36:25 -0700

    add test to convert user input string into a character array

--- Tests/ScrabbleTest.cs
+++ Tests/ScrabbleTest.cs
@@ -5,12 +5,21 @@ namespace ScrabbleProject.Objects
 {
   public class ScrabbleTest
   {
+    // [Fact]
+    // public void ScrabbleScoring_IsScrabbleScore_true()
+    // {
+    //   string thaWords = "pants";
+    //   Scrabble testWord = new Scrabble();
+    //   Assert.Equal(true, testWord.ScrabbleScoring(thaWords));
+    // }
+
     [Fact]
-    public void IsScrabbleScore_true()
+    public void ScrabbleScoring_Separate_true()
     {
       string thaWords = "pants";
+      char[] thaWordsAsArray = { 'p', 'a', 'n', 't', 's' };
       Scrabble testWord = new Scrabble();
-      Assert.Equal(true, testWord.ScrabbleScoring(thaWords));
+      Assert.Equal(thaWordsAsArray, testWord.ScrabbleScoring(thaWords));
     }
   }
 }
